78268a002be819140f6d37aafcc11927e1eefe39
KeboWang <bill.wang@westtown.edu>
2016-07-06 11:28:00 -0400

    Initial Commit

---
+++ AudioRecording/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioRecording
+//
+//  Created by Bill Wang on 7/6/16.
+//  Copyright © 2016 Bill Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
